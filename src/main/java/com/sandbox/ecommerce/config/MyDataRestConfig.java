2062844a96d77e7077374a3f4a4d7c75c32c4b52
David Geraldo <davidgrldo@Davids-MacBook-Pro.local>
2022-05-11 09:27:59 +0700

    move to pc

--- src/main/java/com/sandbox/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/sandbox/ecommerce/config/MyDataRestConfig.java
@@ -1,6 +1,10 @@
 package com.sandbox.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
 import com.sandbox.ecommerce.entity.Product;
 import com.sandbox.ecommerce.entity.ProductCategory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,7 +43,30 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withCollectionExposure(
                         (metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
 
+        // call an internal helper method
+        exposeIds(config);
+
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entites = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entites) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/doman type
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
 }
