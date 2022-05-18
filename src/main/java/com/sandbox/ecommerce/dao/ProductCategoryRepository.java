a88db51c148892c6f0149aec22a29e3b8e879b49
Daniel Niko <daniel.gultom@gmail.com>
2022-05-18 14:50:23 +0700

    pagination

--- src/main/java/com/sandbox/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/sandbox/ecommerce/dao/ProductCategoryRepository.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-
-}
+	//
+}	
